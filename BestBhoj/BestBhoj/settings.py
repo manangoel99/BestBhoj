97c52b81771cef206f239494f5b4fd53fcc31bf6
Manan Goel <manan.goel@research.iiit.ac.in>
2018-06-23 12:55:14 +0530

    Model Created

--- BestBhoj/BestBhoj/settings.py
+++ BestBhoj/BestBhoj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Billing.apps.BillingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- BestBhoj/Billing/models.py
+++ BestBhoj/Billing/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class orders(models.Model):
+
+    name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=250)
+    order = models.CharField(max_length=1000)
+    remarks = models.CharField(max_length=1000)
+    operator = models.CharField(max_length=50)
+    payment_status = models.BooleanField(default=False)
+    amount = models.IntegerField()
+
+    def __str__ (self):
+        return 'Order For ' + self.name 
