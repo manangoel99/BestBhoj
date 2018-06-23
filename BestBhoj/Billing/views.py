347ed0f27948fadbd0bc904883b79a2ab92e0760
Manan Goel <manan.goel@research.iiit.ac.in>
2018-06-23 16:16:47 +0530

    User Authentication Implemented

--- BestBhoj/Billing/views.py
+++ BestBhoj/Billing/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .forms import LogInForm
+
 
 # Create your views here.
 def index(request):
-    if request.method == 'GET':
-        return HttpResponse("HELLO MATE")
+    #context = {}
+    #return render(request, 'Billing/index.html', context)
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            #print(form.cleaned_data)
+            #return HttpResponse("Thanks")
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                return HttpResponse("<h1>Thanks For Logging In</h1>")
+            else:
+                form = LogInForm()
+    else:
+        form = LogInForm()
+    return render(request, 'Billing/index.html', {
+        'form' : form
+        })
